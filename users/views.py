f91d4a02083c157050a9cf9963ae765fd82d957f
Skam <_skam_@mail.ru>
2022-02-24 22:21:12 +0500

    added basket to context filter

--- users/views.py
+++ users/views.py
@@ -48,7 +48,7 @@ def profile(request):
             return HttpResponseRedirect(reverse('users:profile'))
     else:
         form = UserProfileForm(instance=request.user)
-    context = {'title': 'GeekShop - Профиль', 'form': form, 'baskets': Basket.objects.all(), }
+    context = {'title': 'GeekShop - Профиль', 'form': form, 'baskets': Basket.objects.filter(user=request.user), }
     return render(request, 'users/profile.html', context)
 
 
