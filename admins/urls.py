a4b0c3cc5815ba83b3fa385513e6de1610e8edc3
Skam <_skam_@mail.ru>
2022-03-03 17:30:42 +0500

    added controller admins_users_create

--- admins/urls.py
+++ admins/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from admins.views import index, admins_users
+from admins.views import index, admins_users, admins_users_create
 
 
 app_name = 'admins'
@@ -7,4 +7,5 @@ app_name = 'admins'
 urlpatterns = [
     path('', index, name='index'),
     path('users/', admins_users, name='admins_users'),
+    path('users-create/', admins_users_create, name='admins_users_create'),
 ]
--- admins/views.py
+++ admins/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
+
 from users.models import User
 
+from admins.forms import UserAdminRegistrationForm
 
 def index(request):
     context = {"title": 'Geekshop - Aдмин'}
@@ -10,3 +13,16 @@ def admins_users(request):
     user = User.objects.all()
     context = {"title": 'Geekshop - Aдмин', 'users': user}
     return render(request, 'admins/admin-users-read.html', context)
+
+def admins_users_create(request):
+    if request.method == 'POST':
+        form = UserAdminRegistrationForm(data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('admins_staff:admins_users'))
+        else:
+            print(form.errors)
+    else:
+        form = UserAdminRegistrationForm()
+    context = {"title": 'Geekshop - Aдмин', 'form':form}
+    return render(request, 'admins/admin-users-create.html', context)
