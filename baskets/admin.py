e8e08270599cc13dbeeb473f110757214cc2f69a
Skam <_skam_@mail.ru>
2022-03-05 17:00:23 +0500

    fix bag

--- baskets/admin.py
+++ baskets/admin.py
@@ -3,7 +3,6 @@ from django.contrib import admin
 from baskets.models import Basket
 
 
-
 class BasketAdminInLine(admin.TabularInline):
     model = Basket
     fields = readonly_fields = ('product', 'quantity', 'created_timestamp')
