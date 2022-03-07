71fd9f460933c45508d14c9968bb7caabffbc7be
Skam <_skam_@mail.ru>
2022-03-07 12:33:01 +0500

    remove admins def

--- admins/views.py
+++ admins/views.py
@@ -30,13 +30,6 @@ class UserAdminListView(ListView):
         return super(UserAdminListView,self).dispatch(request, *args, **kwargs)
 
 
-#@user_passes_test(lambda u: u.is_staff)
-#def admins_users(request):
-#    user = User.objects.all()
-#    context = {"title": 'Geekshop - Aдмин', 'users': user}
-#    return render(request, 'admins/admin-users-read.html', context)
-
-
 class UserAdminCreateView(CreateView):
     model = User
     form_class = UserAdminRegistrationForm
@@ -51,22 +44,6 @@ class UserAdminCreateView(CreateView):
     @method_decorator(user_passes_test(lambda u: u.is_staff))
     def dispatch(self, request, *args, **kwargs):
         return super(UserAdminCreateView, self).dispatch(request, *args, **kwargs)
-
-
-#@user_passes_test(lambda u: u.is_staff)
-#def admins_users_create(request):
-#    if request.method == 'POST':
-#        form = UserAdminRegistrationForm(data=request.POST, files=request.FILES)
-#        if form.is_valid():
-#            form.save()
-#            messages.success(request, 'Пользователь успешно создан!')
-#            return HttpResponseRedirect(reverse('admins_staff:admins_users'))
-#        else:
-#            print(form.errors)
-#    else:
-#        form = UserAdminRegistrationForm()
-#    context = {"title": 'Geekshop - Aдмин', 'form':form}
-#    return render(request, 'admins/admin-users-create.html', context)
 
 
 class UserAdminUpdateView(UpdateView):
@@ -84,20 +61,6 @@ class UserAdminUpdateView(UpdateView):
     def dispatch(self, request, *args, **kwargs):
         return super(UserAdminUpdateView, self).dispatch(request, *args, **kwargs)
 
-#@user_passes_test(lambda u: u.is_staff)
-#def admins_users_update(request, pk):
-#    selected_user = User.objects.get(id=pk)
-#    if request.method == 'POST':
-#        form = UserAdminProfileForm(instance=selected_user, files=request.FILES, data=request.POST)
-#        if form.is_valid():
-#            form.save()
-#            return HttpResponseRedirect(reverse('admins_staff:admins_users'))
-#    else:
-#        form = UserAdminProfileForm(instance=selected_user)
-#
-#    context = {"title": 'Geekshop - Aдмин', 'form': form, 'selected_user': selected_user}
-#    return render(request, 'admins/admin-users-update-delete.html', context)
-
 
 class UserAdminDeleteView(DeleteView):
     model = User
@@ -112,10 +75,3 @@ class UserAdminDeleteView(DeleteView):
     @method_decorator(user_passes_test(lambda u: u.is_staff))
     def dispatch(self, request, *args, **kwargs):
         return super(UserAdminDeleteView, self).dispatch(request, *args, **kwargs)
-
-
-#@user_passes_test(lambda u: u.is_staff)
-#def admins_users_delete(request, pk):
-#    user = User.objects.get(id=pk)
-#    user.save_delete()
-#    return HttpResponseRedirect(reverse('admins_staff:admins_users'))
