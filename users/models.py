42ced1ea9fad78c9cbc59a88915992d304252f29
Skam <_skam_@mail.ru>
2022-03-26 10:30:10 +0500

    fix bug

--- users/models.py
+++ users/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.timezone import now
+from datetime import timedelta
 
 class User(AbstractUser):
     image = models.ImageField(upload_to='users images', null=True,blank=True)
