e4fce9d052c33407aecf7ffec8bb22345bdf8fa5
Skam <_skam_@mail.ru>
2022-03-26 10:48:20 +0500

    added activation_key

--- users/models.py
+++ users/models.py
@@ -1,8 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.timezone import now
+from datetime import timedelta
+
 
 class User(AbstractUser):
     image = models.ImageField(upload_to='users images', null=True,blank=True)
+    activation_key = models.CharField(max_length=128,blank=True)
+    activation_key_expires = models.DateTimeField(default=(now() + timedelta(hours=48)),null=True,blank=True)
+
+    @property
+    def is_activation_key_expires(self):
+        try:
+            if now() <= self.activation_key_expires:
+                return False
+        except Exception as e:
+            pass
+        return True
+
 
     def save_delete(self):
         self.is_active = False
--- users/views.py
+++ users/views.py
@@ -4,6 +4,7 @@ from django.urls import reverse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
+
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 from baskets.models import Basket
 
